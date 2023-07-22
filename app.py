18d9d9ef6b81defc7a4f50c24749c8ff0b20bb1f
Priscilla Souza <priscillaxtp@edu.unirio.br>
2023-07-21 22:19:43 -0300

    init

---
+++ app.py
@@ -0,0 +1,24 @@
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_list():
+    items = [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+        'Item 5',
+        'Item 6',
+        'Item 7',
+        'Item 8',
+        'Item 9',
+        'Item 10'
+    ]
+    return render_template('list.html', items=items)
+
+if __name__ == '__main__':
+    app.run()
